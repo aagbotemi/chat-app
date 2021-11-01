6e8cab50e617640056a5ef5e9cc998793a814be8
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-11-01 23:29:29 +0100

    join page route added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Join from './components/Join';
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Join} />
+      </Switch>
+    </Router>
   );
 }
 
