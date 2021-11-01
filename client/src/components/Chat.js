66d563d48927fd7a0b10e5e08697757dd8aeaee4
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-11-01 23:14:32 +0100

    chat and join file added

---
+++ client/src/components/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chat = () => {
+    return (
+        <div>
+            This is the Chat section
+        </div>
+    )
+}
+
+export default Chat
---
+++ client/src/components/Join.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Join = () => {
+    return (
+        <div>
+            This is the join section
+        </div>
+    )
+}
+
+export default Join
