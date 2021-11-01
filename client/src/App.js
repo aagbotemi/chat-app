e9f468cd4932d4a931220b3da5381d681543a088
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-11-01 23:30:33 +0100

    chat page route added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Chat from './components/Chat';
 import Join from './components/Join';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={Join} />
+        <Route path="/chat" component={Chat} />
       </Switch>
     </Router>
   );
