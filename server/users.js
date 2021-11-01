dc53fe43e4571305bc3bfa366936ccb1172890b5
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-11-01 23:09:33 +0100

    user functions are exported

--- server/users.js
+++ server/users.js
@@ -25,3 +25,5 @@ const removeUser = (id) => {
 const getUser = (id) => users.find((user) => user.id === id);
 
 const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
